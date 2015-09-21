a63b8f8c9886b53cc75f298533ee91023b587b79
Vignesh <vigneshmanix@gmail.com>
2015-09-21 22:42:50 +0530

    add analytics code

--- resources/views/index1.blade.php
+++ resources/views/index1.blade.php
@@ -12,6 +12,14 @@
       <script src="../bower_components/html5shiv/dist/html5shiv.js"></script>
       <script src="../bower_components/respond/dest/respond.min.js"></script>
     <![endif]-->
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-19500581-1', 'auto');
+      ga('send', 'pageview');
+    </script>
   </head>
   <body>
     <div class="navbar navbar-default navbar-fixed-top">
